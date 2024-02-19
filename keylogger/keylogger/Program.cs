eed1b9d3d682c98399e323360bf980282db96185
reresko <ad.ca@seznam.cz>
2024-02-19 18:15:11 +0100

    added keylogger project

---
+++ keylogger/keylogger/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+//using GlobalLowLevelKeyboardHook;
+
+namespace keylogger
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string filePath = @"c:\Users\adca\Desktop\log.txt";
+            ConsoleKeyInfo keyInfo;
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                do
+                {
+                    keyInfo = Console.ReadKey(intercept: true);
+                    writer.Write($"{keyInfo.KeyChar}");
+
+                } while (keyInfo.Key != ConsoleKey.Escape);
+            }
+            Console.WriteLine("\ninput saved to .txt file");
+        }
+    }
+}
