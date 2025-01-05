69da4a062275bdea5ebf1b06046fa1ec2453b85e
reresko <ad.ca@seznam.cz>
2025-01-05 16:08:40 +0100

    minesweeper winforms fcni

---
+++ Minesweeper WinForms/Minesweeper WinForms/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Minesweeper_WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlBody = new Panel();
+            Restart = new Button();
+            txtFlag = new TextBox();
+            txtScore = new TextBox();
+            SuspendLayout();
+            // 
+            // pnlBody
+            // 
+            pnlBody.BackColor = SystemColors.Control;
+            pnlBody.Location = new Point(41, 121);
+            pnlBody.Name = "pnlBody";
+            pnlBody.Size = new Size(618, 463);
+            pnlBody.TabIndex = 10;
+            // 
+            // Restart
+            // 
+            Restart.Dock = DockStyle.Top;
+            Restart.Location = new Point(0, 0);
+            Restart.Name = "Restart";
+            Restart.Size = new Size(714, 30);
+            Restart.TabIndex = 1;
+            Restart.Text = "Restart";
+            Restart.UseVisualStyleBackColor = true;
+            Restart.Click += btnRestart_Click;
+            // 
+            // txtFlag
+            // 
+            txtFlag.Dock = DockStyle.Top;
+            txtFlag.Location = new Point(0, 30);
+            txtFlag.Name = "txtFlag";
+            txtFlag.Size = new Size(714, 27);
+            txtFlag.TabIndex = 2;
+            // 
+            // txtScore
+            // 
+            txtScore.Dock = DockStyle.Top;
+            txtScore.Location = new Point(0, 57);
+            txtScore.Name = "txtScore";
+            txtScore.Size = new Size(714, 27);
+            txtScore.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(714, 681);
+            Controls.Add(txtScore);
+            Controls.Add(txtFlag);
+            Controls.Add(Restart);
+            Controls.Add(pnlBody);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel pnlBody;
+        private Button Restart;
+        private TextBox txtFlag;
+        private TextBox txtScore;
+    }
+}
---
+++ Minesweeper WinForms/Minesweeper WinForms/Form1.cs
@@ -0,0 +1,258 @@
+namespace Minesweeper_WinForms
+{
+    public partial class Form1 : Form
+    {
+        byte[,] positions = new byte[15, 15];
+        Button[,] buttonList = new Button[15, 15];
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateBombs();
+            GeneratePositionValue();
+            GenerateButtons();
+        }
+
+        Random rng = new Random();
+
+        private void GenerateBombs()
+        {
+            int bombs = 0;
+
+            while (bombs < 30)
+            {
+                int x = rng.Next(0, 14);
+                int y = rng.Next(0, 14);
+
+                if (positions[x, y] == 0)
+                {
+                    positions[x, y] = 10;
+                    bombs++;
+                }
+            }
+        }
+
+        private void GeneratePositionValue()
+        {
+            //2 loopy skrz vsechny pozice
+            for (int x = 0; x < 15; x++)
+            {
+                for (int y = 0; y < 15; y++)
+                {
+                    //pro miny nepocitam okolni miny
+                    if (positions[x, y] == 10)
+                        continue;
+
+                    byte bombCounts = 0;
+
+                    //2 loopy pocitajici kolik bomb je na kazdem sousedovi
+                    for (int counterX = -1; counterX < 2; counterX++)
+                    {
+                        int checkerX = x + counterX;
+
+                        for (int counterY = -1; counterY < 2; counterY++)
+                        {
+                            int checkerY = y + counterY;
+
+                            //out of bound, cili krajni, pro ktere nema smysl pocitat
+                            if (checkerX == -1 || checkerY == -1 || checkerX > 14 || checkerY > 14)
+                                continue;
+
+                            //nasel jsem jako souseda bunku ve ktere jsem, nema smysl pocitat
+                            if (checkerY == y && checkerX == x)
+                                continue;
+
+                            if (positions[checkerY, checkerX] == 10)
+                                bombCounts++;
+                        }
+                    }
+
+                    //prazdne, nebo zadna mina v okoli
+                    if (bombCounts == 0)
+                        positions[x, y] = 20;
+                    else
+                        positions[x, y] = bombCounts;
+                }
+            }
+        }
+
+        private void GenerateButtons()
+        {
+            int xLoc = 3;
+            int yLoc = 6;
+            for (int x = 0; x < 15; ++x)
+            {
+                for (int y = 0; y < 15; ++y)
+                {
+                    Button btn = new Button();
+                    btn.Parent = pnlBody;
+                    btn.Location = new Point(xLoc, yLoc);
+                    btn.Size = new Size(30, 30);
+                    btn.Tag = $"{x},{y}";
+                    btn.Click += BtnClick;
+                    btn.MouseUp += BtnMouseUp;
+                    xLoc += 30;
+                    buttonList[x, y] = btn;
+                }
+
+                yLoc += 30;
+                xLoc = 3;
+            }
+        }
+
+        private void BtnClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            int x = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(0));
+            int y = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(1));
+
+            byte value = positions[x, y];
+
+            //kdyz je bomba tady
+            if (value == 10)
+            {
+                btn.Image = Properties.Resources.mine1;
+
+                MessageBox.Show("Game Over");
+                pnlBody.Enabled = false;
+            }
+
+            //kdyz je to tady prazdny
+            else if (value == 20)
+            {
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderColor = SystemColors.ControlDark;
+                btn.FlatAppearance.BorderSize = 1;
+                btn.Enabled = false;
+
+                OpenAdjacentEmptyFile(btn);
+
+                points++;
+            }
+
+            //kdyz to ma sousedni bomby
+            else
+            {
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderColor = SystemColors.ControlDark;
+                btn.FlatAppearance.MouseDownBackColor = btn.BackColor;
+                btn.FlatAppearance.MouseOverBackColor = btn.BackColor;
+
+                btn.Text = positions[x, y].ToString();
+                points++;
+            }
+
+            btn.Click -= BtnClick;
+            txtScore.Text = points.ToString();
+
+        }
+
+        private void OpenAdjacentEmptyFile(Button btn)
+        {
+            int x = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(0));
+            int y = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(1));
+
+            List<Button> emptyButtons = new List<Button>();
+
+            for (int counterX = -1; counterX < 2; counterX++)
+            {
+                int checkerX = x + counterX;
+
+                for (int counterY = -1; counterY < 2; counterY++)
+                {
+                    int checkerY = y + counterY;
+
+                    //out of bound, cili krajni, pro ktere nema smysl pocitat
+                    if (checkerX == -1 || checkerY == -1 || checkerX > 14 || checkerY > 14)
+                        continue;
+
+                    //nasel jsem jako souseda bunku ve ktere jsem, nema smysl pocitat
+                    if (checkerY == y && checkerX == x)
+                        continue;
+
+                    Button btnAdjacent = buttonList[checkerX, checkerY];
+
+                    int xAdjacent = Convert.ToInt32(btnAdjacent.Tag.ToString().Split(',').GetValue(0));
+                    int yAdjacent = Convert.ToInt32(btnAdjacent.Tag.ToString().Split(',').GetValue(1));
+
+                    byte value = positions[xAdjacent, yAdjacent];
+
+                    if (value == 20)
+                    {
+                        //zjisti jestli btnAdjacent uz byl otevren nebo jestli na nej bylo kliknuto
+                        if (btnAdjacent.FlatStyle != FlatStyle.Flat)
+                        {
+                            btnAdjacent.FlatStyle = FlatStyle.Flat;
+                            btnAdjacent.FlatAppearance.BorderSize = 1;
+                            btnAdjacent.FlatAppearance.BorderColor = SystemColors.ControlDark;
+                            btnAdjacent.Enabled = false;
+                            emptyButtons.Add(btnAdjacent);
+                            points++;
+                        }
+
+                    }
+
+                    else if (value != 10)
+                    {
+                        btnAdjacent.PerformClick();
+                    }
+                }
+            }
+
+            foreach (var btnEmpty in emptyButtons)
+            {
+                OpenAdjacentEmptyFile(btnEmpty);
+            }
+
+            txtScore.Text = points.ToString();
+        }
+
+        int flag = 30;
+        int points = 0;
+
+        private void BtnMouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Button btn = (Button)sender;
+
+                if (btn.Image == null)
+                {
+                    btn.Image = Properties.Resources.red_flag1;
+                    btn.Click -= BtnClick;
+                    flag--;
+                }
+                else
+                {
+                    btn.Image = null;
+                    btn.Click += BtnClick;
+                    flag++;
+                }
+                txtFlag.Text = flag.ToString();
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            points = 0;
+            flag = 30;
+
+            for (int x = 0; x < 15; x++)
+            {
+                for (int y = 0; y < 15; y++)
+                {
+                    buttonList[x, y].Dispose();
+                }
+            }
+
+            pnlBody.Controls.Clear();
+            pnlBody.Enabled = true;
+            buttonList = new Button[15, 15];
+            positions = new byte[15, 15];
+
+            GenerateBombs();
+            GeneratePositionValue();
+            GenerateButtons();
+        }
+    }
+}
