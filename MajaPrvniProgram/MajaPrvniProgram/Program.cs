872d07222b5b578e1f03a8cc2c84f4984326a50c
reresko <ad.ca@seznam.cz>
2025-01-09 22:32:45 +0100

    MajaPrvniProgram created

---
+++ MajaPrvniProgram/MajaPrvniProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MajaPrvniProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("fhfggfgf");
+            Console.ReadKey();
+        }
+    }
+}
