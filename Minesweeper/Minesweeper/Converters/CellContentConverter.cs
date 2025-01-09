9da13514d3adfbb3049728682315659e393c434a
reresko <ad.ca@seznam.cz>
2025-01-09 22:33:04 +0100

    some changes

---
+++ Minesweeper/Minesweeper/Converters/CellContentConverter.cs
@@ -0,0 +1,32 @@
+﻿using Minesweeper.Model;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Minesweeper.Converters
+{
+    public class CellContentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Cell cell)
+            {
+                if (cell.IsFlagged)
+                    return "🚩"; // Flag icon
+                if (!cell.IsRevealed)
+                    return ""; // Covered cell
+                if (cell.IsMine)
+                    return "💣"; // Mine icon
+                if (cell.AdjacentMines > 0)
+                    return cell.AdjacentMines.ToString(); // Number of adjacent mines
+                return ""; // Blank revealed cell
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Minesweeper/Minesweeper/Model/Cell.cs
+++ Minesweeper/Minesweeper/Model/Cell.cs
@@ -1,18 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Minesweeper.Model
 {
-    public class Cell
+    public class Cell : INotifyPropertyChanged
     {
         public int Row { get; set; }
         public int Column { get; set; }
         public bool IsMine { get; set; }
         public int AdjacentMines { get; set; }
-        public bool IsRevealed { get; set; }
-        public bool IsFlagged { get; set; }
+        public bool _isRevealed { get; set; }
+        public bool _isFlagged { get; set; }
+
+        public bool IsRevealed
+        {
+            get => _isRevealed;
+            set
+            {
+                if (_isRevealed != value)
+                {
+                    _isRevealed = value;
+                    OnPropertyChanged(nameof(IsRevealed));
+                }
+            }
+        }
+
+        public bool IsFlagged
+        {
+            get => _isFlagged;
+            set
+            {
+                if (_isFlagged != value)
+                {
+                    _isFlagged = value;
+                    OnPropertyChanged(nameof(IsFlagged));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
